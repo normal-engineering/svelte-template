422934d4f63c846109d0df12f662a145e25a56a5
kwsu30 <50939503+kwsu30@users.noreply.github.com>
2025-07-22 14:00:02 +0800

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,8 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: { 	adapter: adapter(), 
+			prerender: { default: true } }
 };
 
 export default config;
