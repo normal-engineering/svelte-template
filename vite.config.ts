ccef7032c7e1558c20df1c6d967fa7a97db12141
kwsu30 <50939503+kwsu30@users.noreply.github.com>
2025-07-22 13:39:51 +0800

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	server: {
+		host: '0.0.0.0',
+		port: 5173
+	},
 	plugins: [tailwindcss(), sveltekit()]
 });
